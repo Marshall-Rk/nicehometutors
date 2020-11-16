a0e1aeeb8fef8a295113eef740db013d6df699bc
Marshall-Rk <57613854+Marshall-Rk@users.noreply.github.com>
2020-11-16 20:42:00 +0530

    more with form

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rajkamalgautam2001@gmail.com";
+    $to = "rockybd1995@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
