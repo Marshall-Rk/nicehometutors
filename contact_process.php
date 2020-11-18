df0838f7f5b2f53b2fcb1f5271f7322e719041f0
Marshall-Rk <57613854+Marshall-Rk@users.noreply.github.com>
2020-11-18 18:07:50 +0530

    21

--- contact_process.php
+++ contact_process.php
@@ -15,5 +15,5 @@ $recipient = "rajkamalgautam2001@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "<h3>Thank you for contacting us – we will get back to you soon!</h3>";
+echo "<h4>Thank you for contacting us – we will get back to you soon!</h4>";
 ?>  
