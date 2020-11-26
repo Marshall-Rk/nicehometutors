79b40b68889afa5f620109759a9be7e30009bcbc
Marshall-Rk <57613854+Marshall-Rk@users.noreply.github.com>
2020-11-26 18:19:43 +0530

    yes

--- student form.php
+++ student form.php
@@ -1,6 +1,39 @@
 
 
-<?php 
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$sender_email = "";
+$sender_password = "";
+function sendMail($to, $subject, $msg)
+{
+    global  $sender_password, $sender_email;
+
+    $mail = new PHPMailer(true);
+    try {
+        $mail->SMTPDebug = 1;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $sender_email;
+        $mail->Password = $sender_password;
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+        $mail->setFrom($sender_email, "Maple Space");
+        $mail->addAddress($to);
+        $mail->isHTML(true);
+        $mail->Subject = "$subject";
+        $mail->Body = "<h1 style='color:#ff3300'>$msg</h1>";
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        $error = "There was an error while sending the mail but your registartion request has been submitted to us.";
+        return false;
+    }
+}
+
 
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
@@ -18,11 +51,11 @@ $when = $_POST['when'];
 $leclocation = $_POST['leclocation'];
 
 
-$formcontent="Student Registration From: \n  $first_name $last_name \n Birthday: $birthday  \n E-mail: $email \n Location: $location \n Address: $address \n Phone no: $phone1 $phone2   \n Course: $course \n Board: $board \n Session: $session \n gender: $gender \n when: $when \n Lecture location: $leclocation  ";
+$formcontent = "Student Registration From: \n  $first_name $last_name \n Birthday: $birthday  \n E-mail: $email \n Location: $location \n Address: $address \n Phone no: $phone1 $phone2   \n Course: $course \n Board: $board \n Session: $session \n gender: $gender \n when: $when \n Lecture location: $leclocation  ";
 
 $recipient = "rajkamalgautam2001@gmail.com";
 $subject = "Student Registration Form";
-$mailheader = "From: $email \r\n";
-mail($recipient,  $formcontent, $mailheader) or die("Error!");
+
+sendMail($recipient,  $formcontent, $mailheader);
 echo "<h4>Thank you for contacting us – we will get back to you soon!</h4>";
 ?>  
